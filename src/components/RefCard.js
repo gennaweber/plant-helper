95244404d8bb76e9035fbf486a3178ca6c231d2a
Genna <gennamweber@gmail.com>
2023-05-21 11:17:48 -0400

    Add custom link fetcher

--- src/components/RefCard.js
+++ src/components/RefCard.js
@@ -1,12 +1,32 @@
-import Microlink from "@microlink/react";
 import { Card, Grid, Typography } from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
 
 const RefCard = ({ description, link, category }) => {
+  const [ref, inView] = useInView();
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    if (inView && data === null) {
+      const customFetcher = async () => {
+        try {
+          const response = await fetch(
+            `https://link-preview-tb5v.onrender.com/v2?url=${link}`
+          );
+          const json = await response.json();
+          return json.metadata;
+        } catch (error) {
+          console.error(error);
+        }
+      };
+      customFetcher().then((res) => setData(res));
+    }
+  }, [inView]);
+
   return (
     <Grid item xs={12} md={6} lg={4}>
-      <Card sx={{ minHeight: 100 }}>
-        {link && <Microlink url={link} />}
+      <Card sx={{ minHeight: 100 }} ref={ref}>
+        {data ? data.title : link}
         <Grid
           container
           sx={{ minHeight: 100 }}
--- src/components/TabsRouter.js
+++ src/components/TabsRouter.js
@@ -1,8 +1,8 @@
-import { AppBar } from '@mui/material';
-import Box from '@mui/material/Box';
-import Tab from '@mui/material/Tab';
-import Tabs from '@mui/material/Tabs';
-import { Link, matchPath, useLocation } from 'react-router-dom';
+import { AppBar } from "@mui/material";
+import Box from "@mui/material/Box";
+import Tab from "@mui/material/Tab";
+import Tabs from "@mui/material/Tabs";
+import { Link, matchPath, useLocation } from "react-router-dom";
 
 function useRouteMatch(patterns) {
   const { pathname } = useLocation();
@@ -18,53 +18,47 @@ function useRouteMatch(patterns) {
   return null;
 }
 
-function MyTabs() {
+const MyTabs = () => {
   // You need to provide the routes in descendant order.
   // This means that if you have nested routes like:
   // users, users/new, users/edit.
   // Then the order should be ['users/add', 'users/edit', 'users'].
   const routeMatch = useRouteMatch([
-    '/',
-    '/dictionary',
-    '/refs',
-    '/collection/:name',
-    '/collection',
+    "/",
+    "/refs",
+    "/collection/:name",
+    "/collection",
   ]);
   const currentTab = routeMatch?.pattern?.path;
 
   return (
     <Tabs
       value={currentTab}
-      variant='fullWidth'
-      indicatorColor='secondary'
-      aria-label='Navigation tabs'
-      textColor='inherit'>
-      <Tab label='Care Guide' value='/' to='/' component={Link} />
+      variant="fullWidth"
+      indicatorColor="secondary"
+      aria-label="Navigation tabs"
+      textColor="inherit"
+    >
+      <Tab label="Care Guide" value="/" to="/" component={Link} />
+      <Tab label="References" value="/refs" to="/refs" component={Link} />
       <Tab
-        label='Dictionary'
-        value='/dictionary'
-        to='/dictionary'
-        component={Link}
-      />
-      <Tab label='References' value='/refs' to='/refs' component={Link} />
-      <Tab
-        value='/collection/:name'
-        to='/collection'
-        style={{ display: 'none' }}
+        value="/collection/:name"
+        to="/collection"
+        style={{ display: "none" }}
       />
       <Tab
-        label='My Collections'
-        value='/collection'
-        to='/collection'
+        label="My Collections"
+        value="/collection"
+        to="/collection"
         component={Link}
       />
     </Tabs>
   );
-}
+};
 
 export default function TabsRouter() {
   return (
-    <Box sx={{ width: '100%', bgcolor: 'primary', color: '#fff' }}>
+    <Box sx={{ width: "100%", bgcolor: "primary", color: "#fff" }}>
       <AppBar>
         <MyTabs />
       </AppBar>
